9b195ad2afa5c5b6379a4e1ebca7897a840e9d45
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-04-10 01:13:04 +0200

    Changes the way to verify the github signature (once again)

--- web/index.js
+++ web/index.js
@@ -2,34 +2,43 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bl = require('bl');
 const crypto = require('crypto');
 
 module.exports = function(eventEmitter, secret) {
   var app = express();
-  app.use(function(req, res, next) {
-    const signature = req.get('X-Hub-Signature');
 
-    req.pipe(bl((err, data) => {
-      const sha1 = 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
-      console.log('verifySignature', sha1, signature);
-      if (sha1 === signature) {
-        next();
-      }
-      else {
-        res.status(400).send('Invalid signature');
-      }
-    }));
-  });
+  function signBlob(key, blob) {
+    return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex');
+  }
 
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({
+    extended: true,
+    verify: function(req, res, buffer) {
+      if (!req.get('x-hub-signature'))
+        throw new Error('No X-Hub-Signature found on request');
+
+      if (!req.get('x-github-event'))
+        throw new Error('No X-Github-Event found on request');
+
+      var signature = req.get('x-hub-signature');
+      var computedSignature = signBlob(secret, buffer);
+
+      console.log('verified', signature, computedSignature);
+
+      if (signature !== computedSignature) {
+        console.warn('Recieved an invalid HMAC: calculated:' +
+          computedSignature + ' != recieved:' + signature);
+        throw new Error('Invalid Signature');
+      }
+    }
+  }));
 
   app.get('/', (req, res) => {
     res.status(200).send('pong!');
   });
 
   app.post('/ghwebook', (req, res) => {
-    const event = req.get('X-GitHub-Event');
+    const event = req.get('x-github-event');
     eventEmitter.emit(event, req.body.payload);
     res.status(200).send('ok');
   });
