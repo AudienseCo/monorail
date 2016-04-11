bc02c267542a793b2e49c8e071a7aa99432b3e54
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-04-11 20:52:31 +0200

    Subscribe the deploy notes checker to the label events

--- checkers/deploy-notes/deployNotesSubscriber.js
+++ checkers/deploy-notes/deployNotesSubscriber.js
@@ -4,14 +4,14 @@ module.exports = function(emitter, deployNotesService) {
   let that = {
     subscribe: () => {
       emitter.on('pull_request', payload => {
-        if (payload.action === 'opened') {
+        if (payload.action === 'opened' || payload.action === 'labeled' ||
+        payload.action === 'unlabeled') {
           const prInfo = payload.pull_request;
           deployNotesService.updatePullRequestCommit(prInfo);
         }
       });
 
       emitter.on('issue_comment', payload => {
-        console.log('issue_comment');
         if (payload.action === 'created' && payload.issue && payload.issue.pull_request) {
           if (payload.comment.body.toLowerCase() === 'check deploy notes please') {
             const prInfo = payload.issue;
