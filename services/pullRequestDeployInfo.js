bc8cfcdea25f258044188547c9e72da93f49a950
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-04-20 14:04:34 +0200

    Adds the pullRequestDeployInfo service

---
+++ services/pullRequestDeployInfo.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function createPullRequestDeployInfo(github) {
+  var that = {};
+
+  that.get = (prId, cb) => {
+    const deployNotesTag = 'deploy notes';
+    const deployTagRegex = /^deploy (.+)/;
+
+    github.getIssueLabels(prId, (err, labels) => {
+      cb(null, {
+        deployNotes: labels.some(label => {
+          return label.name.toLowerCase() === deployNotesTag;
+        }),
+
+        services:
+          labels.filter(label => {
+            return label.name.match(deployTagRegex);
+          })
+          .map(label => {
+            return label.name.match(deployTagRegex)[1];
+          })
+      });
+    });
+  };
+
+  return that;
+}
---
+++ test/services/pullRequestDeployInfo_spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+require('should');
+const createPullRequestDeployInfo = require('../../services/pullRequestDeployInfo');
+
+describe('Get pull request deploy info', () => {
+  context('Interface', () => {
+
+    const prDeployInfo = createPullRequestDeployInfo();
+
+    it('should have the "get" method', () => {
+      prDeployInfo.get.should.be.a.Function();
+    });
+
+  });
+
+  context('Behaviour', () => {
+
+    function createGithubDummy(response) {
+      return {
+        getIssueLabels: (id, cb) => {
+          cb(null, response);
+        }
+      };
+    }
+
+    it('should get if the issue has deploy notes', (done) => {
+      const githubDummy = createGithubDummy([{ name: 'Deploy notes' }]);
+      const prDeployInfo = createPullRequestDeployInfo(githubDummy);
+
+      prDeployInfo.get(1234, (err, info) => {
+        info.deployNotes.should.be.ok();
+        done();
+      });
+    });
+
+    it('should get the services to deploy', (done) => {
+      const githubDummy = createGithubDummy([
+        { name: 'deploy globalreports'},
+        { name: 'deploy sync-as'}
+      ]);
+      const prDeployInfo = createPullRequestDeployInfo(githubDummy);
+
+      prDeployInfo.get(1234, (err, info) => {
+        info.services.should.be.eql(['globalreports', 'sync-as']);
+        done();
+      });
+    });
+  });
+});
