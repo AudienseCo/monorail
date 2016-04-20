d5a9afaf6e9a47a4a2fad0fd8193476dd548d1a3
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-04-20 19:15:25 +0200

    Fixes the getPullRequestsDeployInfo action to iterate over all the PR ids

--- core/actions/getPullRequestsDeployInfo.js
+++ core/actions/getPullRequestsDeployInfo.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const async = require('async');
+
 module.exports = function(prDeployInfo) {
   return function(ids, cb) {
-    prDeployInfo.get(ids, cb);
+
+    const initial = {
+      deployNotes: false,
+      services: []
+    };
+
+    async.reduce(ids, initial, (acc, id, next) => {
+      prDeployInfo.get(id, (err, prInfo) => {
+        if (err) return next(err);
+
+        const newState = {
+          deployNotes: acc.deployNotes || prInfo.deployNotes,
+          services: Array.from(new Set(acc.services.concat(prInfo.services)))
+        };
+        next(null, newState);
+
+      });
+    }, cb);
   };
 };
--- test/core/actions/getPullRequestsDeployInfo_spec.js
+++ test/core/actions/getPullRequestsDeployInfo_spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('should');
+const sinon = require('sinon');
 
 const createPullRequestDeployInfo = require('../../../core/services/pullRequestDeployInfo');
 const createGetPullRequestsDeployInfo = require('../../../core/actions/getPullRequestsDeployInfo');
@@ -23,10 +24,10 @@ describe('Get pull requests deploy info action', () => {
     }
 
     it('should get the issues deploy info', (done) => {
-      const githubDummy  = createGithubDummy([
-        { name: 'deploy-to:tasks-as' },
-        { name: 'deploy-to:globalreports' }
-      ]);
+      const githubDummy  = createGithubDummy();
+      const stub = sinon.stub(githubDummy, 'getIssueLabels');
+      stub.onFirstCall().callsArgWith(1, null, [{ name: 'deploy-to:tasks-as' }]);
+      stub.onSecondCall().callsArgWith(1, null, [{ name: 'deploy-to:globalreports' }]);
       const prDeployInfo = createPullRequestDeployInfo(githubDummy);
       const getPullRequestsDeployInfo = createGetPullRequestsDeployInfo(prDeployInfo);
       getPullRequestsDeployInfo([1234, 4321], (err, info) => {
