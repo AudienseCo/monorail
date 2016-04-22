3f798eaea19359564a8b435d323316749ffee83a
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-04-22 10:26:03 +0200

    Fixes endpoint method

--- web/private/index.js
+++ web/private/index.js
@@ -26,7 +26,7 @@ module.exports = function(actions) {
 
   });
 
-  app.get('/create-release', (req, res) => {
+  app.post('/create-release', (req, res) => {
     if (!req.body.tag)
       return res.status(400).send({ error: 'You must include the "tag" parameter' });
 
