4bd8fec0d24f491c5b4c51460fd3215fe71be177
Iván Guardado Castro <dev.ivangc@gmail.com>
2016-05-04 13:08:50 +0200

    Logs the requests in the web servers

--- web/private/index.js
+++ web/private/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const express = require('express');
+const morgan  = require('morgan');
 const bodyParser = require('body-parser');
 
 module.exports = function(actions) {
   const app = express();
 
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  app.use(morgan(`[private] :remote-addr - :remote-user [:date[clf]] ":method :url \
+    HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"`));
 
   app.get('/', (req, res) => {
     res.status(200).send('pong!');
