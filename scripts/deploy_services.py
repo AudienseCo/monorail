05e3808ff0f7e4c60bfb03050d52ea185a5e506c
Iván del Castillo Zamora <ivan.castillo@socialbro.com>
2016-05-05 14:52:57 +0200

    Add polling to check the build status in Jenkins

--- scripts/deploy_services.py
+++ scripts/deploy_services.py
@@ -3,14 +3,16 @@ import requests
 import json
 import sys
 import argparse
+import time
+from pprint import pprint
 
 parser = argparse.ArgumentParser('Queries Monorail to get the services and deployNotes where a list of Pull requests will be deployed')
-parser.add_argument('--monorail', help='URL where Monorail is located')
-parser.add_argument('--pullrequests', help='Comma or space separated list of pull requests we will ask for')
-parser.add_argument('--tag', help='Tag associated to the deployment')
-parser.add_argument('--statics', help='Statics version associated to the deployment')
-parser.add_argument('--jenkinsURL', help='URL where Jenkins is located. Defaults to HTTPS if protocol is not entered!')
-parser.add_argument('--jenkinsJob', help='Jenkins job that will be triggered')
+parser.add_argument('--monorail', help='URL where Monorail is located', required=True)
+parser.add_argument('--pullrequests', help='Comma or space separated list of pull requests we will ask for', required=True)
+parser.add_argument('--tag', help='Tag associated to the deployment', required=True)
+parser.add_argument('--statics', help='Statics version associated to the deployment', required=True)
+parser.add_argument('--jenkinsURL', help='URL where Jenkins is located. Defaults to HTTPS if protocol is not entered!', required=True)
+parser.add_argument('--jenkinsJob', help='Jenkins job that will be triggered', required=True)
 parser.add_argument('--jenkinsUser', help='Jenkins user for authentication')
 parser.add_argument('--jenkinsApiToken', help='Jenkins API token for authentication')
 parser.add_argument('--jenkinsJobToken', help='Jenkins Job token for authentication')
@@ -68,13 +70,14 @@ except:
 
 data = r.json()
 services = data['services']
+polling_delay = 15 #seconds
 
 for batch_same_node_version in services:
     payload = {}
     payload['token'] = args.jenkinsJobToken
     payload['cause'] = 'Build triggered with love by Monorail'
     payload['branch'] = tagVersion
-    payload['grunt'] = batch_same_node_version['grunt']
+    payload['grunt'] = str(batch_same_node_version['grunt']).lower()
     payload['node_version'] = batch_same_node_version['nodeVersion']
 
     statics = batch_same_node_version['statics']
@@ -88,9 +91,40 @@ for batch_same_node_version in services:
 
     try:
         r = requests.post(request_jenkins, auth=(args.jenkinsUser, args.jenkinsApiToken), params=payload)
-        print r.text
+        poll_url = r.headers.get('location')
     except:
-        print 'Something went wrong querying Jenkins'
+        print 'There was an error triggering the build in Jenkins'
         exit(3)
 
-exit (0)
+    build_url = ''
+    while (build_url == ''):
+        try:
+            r_poll = requests.get(poll_url + 'api/json', auth=(args.jenkinsUser, args.jenkinsApiToken))
+            r_poll_data = r_poll.json()
+        except:
+            print 'There was an error querying Jenkins about the status of the build'
+            exit(4)
+
+        try:
+            build_url = r_poll_data['executable']['url']
+        except:
+            print r_poll_data['why']
+        time.sleep(polling_delay)
+
+    building = True
+    while (building != False):
+        try:
+            r_build = requests.get(build_url + 'api/json', auth=(args.jenkinsUser, args.jenkinsApiToken))
+            r_build_data = r_build.json()
+            building = r_build_data['building']
+            print "%s: Still building" % r_build_data['fullDisplayName']
+        except:
+            print 'There was an error querying Jenkins about the status of the build'
+            exit(4)
+        time.sleep(polling_delay)
+
+    print "%s: %s" % (r_build_data['fullDisplayName'], r_build_data['result'])
+    if (r_build_data['result'] == "SUCCESS"):
+        exit (0)
+    else:
+        exit (5)
