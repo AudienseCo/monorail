34aaeed59db77c07cc219e963e85f136729e241a
Alfredo Artiles <aartiles@gmail.com>
2019-04-05 19:52:07 +0200

    Combine driver params with local config defaults

--- core/services/build.js
+++ core/services/build.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { eachSeries } = require('async');
-const { get, assignWith, cloneDeep, isNil } = require('lodash');
+const { get, assignWith, cloneDeep, isNil, defaultsDeep } = require('lodash');
 
 module.exports = (callCIDriver, localConfig) => {
   return (branch, jobs, deployConfig,  cb) => {
@@ -15,7 +15,7 @@ module.exports = (callCIDriver, localConfig) => {
       if (!ciJobConfig) return nextJob();
 
       const settings = combineSettings(ciJobConfig.ciService, ciServicesConfig, localConfig);
-      const params = combineParams(job, ciJobConfig, branch);
+      const params = combineParams(job, branch, ciJobConfig, localConfig);
 
       callCIDriver(ciJobConfig.ciService, settings, params, nextJob);
       // TODO: return job execution result
@@ -25,26 +25,27 @@ module.exports = (callCIDriver, localConfig) => {
   function combineSettings(ciService, ciServicesConfig, localConfig) {
     const repoSettings = get(ciServicesConfig, `${ciService}.settings`);
     const localSettings = get(localConfig, `deploy.ciServices.${ciService}.settings`);
-    const settings = applyDefaults(repoSettings, localSettings);
+    const settings = defaultsDeep({}, repoSettings, localSettings);
     return settings;
   }
 
-  function combineParams(job, ciJobConfig, branch) {
-    // TODO: combine default params with monorail config
-    const params = applyDefaults(job.params, ciJobConfig.defaultParams);
+  function combineParams(job, branch, repoCIJobConfig, localConfig) {
+    const localCIJobConfig = get(localConfig, `deploy.ciJobs.['${job.name}']`);
+    const finalCIJobConfig = defaultsDeep({}, repoCIJobConfig, localCIJobConfig);
+    const params = applyBooleanDefaults(job.params, finalCIJobConfig.defaultParams);
 
-    if (ciJobConfig.servicesParam) {
-      params[ciJobConfig.servicesParam.paramName] = job.deployTo.join(ciJobConfig.servicesParam.separator);
+    if (finalCIJobConfig.servicesParam) {
+      params[finalCIJobConfig.servicesParam.paramName] = job.deployTo.join(finalCIJobConfig.servicesParam.separator);
     }
 
-    if (ciJobConfig.sourceVersionParam) {
-      params[ciJobConfig.sourceVersionParam.paramName] = branch;
+    if (finalCIJobConfig.sourceVersionParam) {
+      params[finalCIJobConfig.sourceVersionParam.paramName] = branch;
     }
 
     return params
   }
 
-  function applyDefaults(originalObj, defaultObj) {
+  function applyBooleanDefaults(originalObj, defaultObj) {
     const finalObj = cloneDeep(defaultObj);
     return assignWith(finalObj, originalObj, (defaultProp, originalProp) => {
       return isNil(originalProp) ? defaultProp : originalProp;
--- test/core/services/build_spec.js
+++ test/core/services/build_spec.js
@@ -125,7 +125,88 @@ describe('Build service', () => {
     });
   });
 
-  function createDummyConfig({ jenkinsSettings }) {
+  it('call the jenkins build driver getting params from local config', (done) => {
+    const ciDrivers = {
+      jenkins: (settings, params, cb) => cb()
+    };
+    const callCIDriver = createCallCIDriver(ciDrivers);
+    const callCIDriverSpy = sinon.spy(callCIDriver);
+    const dummyCIJobConfig = {
+      "ciService": "jenkins",
+      "jobName": "monorail-tarball-ecs",
+      "servicesParam": {
+        "paramName": "other_where_to_deploy",
+        "separator": "|"
+      },
+      "sourceVersionParam": {
+        "paramName": "branch"
+      },
+      "defaultParams": {
+        "token": "job token",
+        "branch": "dev",
+        "node_version": "v8.6.0",
+        "grunt": false,
+        "static_files_version": "",
+        "statics": false,
+        "new param": "value"
+      }
+    };
+    const dummyConfig = createDummyConfig({ dummyCIJobConfig });
+    const build = createBuild(callCIDriverSpy, dummyConfig);
+
+    const branch = 'deploy-branch1';
+    const jobs = [
+      {
+        name: 'nodejs v8.6.0',
+        deployTo: ['task-as', 'globalreports-as'],
+        params: {
+          grunt: true,
+          static_files_version: 'other',
+          token: 'other token'
+        }
+      },
+      {
+        name: 'nodejs v10.0.0',
+        deployTo: ['dashboard-as', 'task-as'],
+        params: {
+          token: '',
+          statics: true
+        }
+      }
+    ];
+    const deployConfig = cloneDeep(repoConfig.deploy);
+    delete deployConfig.ciJobs['nodejs v8.6.0'].servicesParam;
+    build(branch, jobs, deployConfig, (err) => {
+      should.not.exist(err);
+      callCIDriverSpy.calledTwice.should.be.ok();
+      callCIDriverSpy.firstCall.args[0].should.be.eql('jenkins');
+      callCIDriverSpy.firstCall.args[1].should.be.eql(deployConfig.ciServices.jenkins.settings);
+      callCIDriverSpy.firstCall.args[2].should.be.eql({
+        token: 'other token',
+        branch: 'deploy-branch1',
+        node_version: 'v8.6.0',
+        grunt: true,
+        static_files_version: 'other',
+        statics: false,
+        other_where_to_deploy: 'task-as|globalreports-as',
+        'new param': 'value'
+      });
+      callCIDriverSpy.secondCall.args[0].should.be.eql('jenkins');
+      callCIDriverSpy.secondCall.args[1].should.be.eql(deployConfig.ciServices.jenkins.settings);
+      callCIDriverSpy.secondCall.args[2].should.be.eql({
+        token: '',
+        branch: 'deploy-branch1',
+        node_version: 'v10.0.0',
+        grunt: false,
+        static_files_version: '',
+        statics: true,
+        where_to_deploy: 'dashboard-as,task-as'
+      });
+      done();
+    });
+  });
+
+  function createDummyConfig({ jenkinsSettings, dummyCIJobConfig }) {
     const dummyConfig = {};
     if (jenkinsSettings) {
       dummyConfig.deploy = {
@@ -134,6 +215,13 @@ describe('Build service', () => {
             driver: 'jenkins',
             settings: jenkinsSettings
           }
+        }
+      };
+    }
+    if (dummyCIJobConfig) {
+      dummyConfig.deploy = {
+        ciJobs: {
+          'nodejs v8.6.0': dummyCIJobConfig,
         }
       };
     }
