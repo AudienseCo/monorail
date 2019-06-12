fd74cbeafac990e0024d503d70aadb3fde70c54d
Alfredo Artiles <aartiles@gmail.com>
2019-06-12 13:22:00 +0200

    Add debug logs to issueReleaseInfo service

--- core/services/issueReleaseInfoList.js
+++ core/services/issueReleaseInfoList.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const async = require('async');
+const logger = require('../../lib/logger');
 
 module.exports = function issueReleaseInfoListBuilder(issueReleaseInfo) {
   const that = {};
 
   that.get = (repo, ids, callback) => {
     async.map(ids, (id, nextId) => {
+      logger.debug('issueReleaseInfo.getInfo', { repo, id });
       issueReleaseInfo.getInfo(repo, id, (err, issueReleaseInfo) => {
         nextId(err, issueReleaseInfo);
       });
