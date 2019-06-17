5bd7b25930a30ac53656fcc603127dea9977c901
Alfredo Artiles <aartiles@gmail.com>
2019-06-17 13:25:30 +0200

    Add logs to callCIDrivers

--- core/services/callCIDriver.js
+++ core/services/callCIDriver.js
@@ -1,12 +1,16 @@
 'use strict';
+const logger = require('../../lib/logger');
 
 module.exports = (ciDrivers) => {
   return (driverName, settings, jobName, params, cb) => {
     const ciDriver = ciDrivers[driverName];
     if (!ciDriver) return cb(new Error('invalid CI driver'));
     ciDriver(settings, jobName, params, (err, success) => {
-      if (err) return cb(err);
-      if (!success) return cb(new Error('CI job failed'));
+      const error = !success && !err ? new Error('CI job failed') : err;
+      if (error) {
+        logger.error('CI job failed', { error, driverName, settings, jobName, params });
+        return cb(error);
+      }
       cb();
     });
   };
