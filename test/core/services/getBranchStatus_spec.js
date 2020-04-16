05827af89e4f335a83b352b5585e92965144ed64
ant30 <antonio.perezaranda@audiense.com>
2020-04-16 17:45:24 +0200

    Remove all the queries to status endpoints, leave the sha reference

--- test/core/services/getBranchStatus_spec.js
+++ test/core/services/getBranchStatus_spec.js
@@ -3,25 +3,18 @@
 const should = require('should');
 const sinon = require('sinon');
 const createGetBranchStatus = require('../../../core/services/getBranchStatus');
-const POLLING_INTERVAL_MS = 1;
 
 describe('get branch status service', () => {
-  it('should succeed when no checks required', (done) => {
+  it('should succeed when branch exists', (done) => {
     const aSha = '123';
     const githubDummy = createGithubDummy(null, {
       getBranchRes: {
         object: {
           sha: aSha
         }
-      },
-      getProtectedBranchRequiredStatusChecksError: {
-        status: 404
-      },
-      getChecksForRefRes: {
-        check_runs: []
       }
     });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
+    const getBranchStatus = createGetBranchStatus(githubDummy);
 
     const repo = 'audienseCo';
     const branch = 'staging';
@@ -41,54 +34,7 @@ describe('get branch status service', () => {
         object: {}
       }
     });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.exists(err);
-      githubDummy.wasCalled('getProtectedBranchRequiredStatusChecks').should.not.be.true();
-      done();
-    });
-  });
-
-  it('shouldnt call gitCheckForRef when no checks required', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksError: {
-        status: 404
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.not.exists(err);
-      sha.should.be.eql(aSha);
-      githubDummy.wasCalled('gitCheckForRef').should.not.be.true();
-      done();
-    });
-  });
-
-  it('should fail when github reply with status not equal to 200 or 404', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksError: {
-        status: 403
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
+    const getBranchStatus = createGetBranchStatus(githubDummy);
 
     const repo = 'audienseCo';
     const branch = 'staging';
@@ -98,212 +44,12 @@ describe('get branch status service', () => {
     });
   });
 
-
-  it('should succeed when no checks required or executed', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: []
-      },
-      getChecksForRefRes: {
-        check_runs: []
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.not.exists(err);
-      sha.should.be.eql(aSha);
-      done();
-    });
-  });
-
-  it('should succeed when all require checks have succeed and are completed', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: ['check1']
-      },
-      getChecksForRefRes: {
-        check_runs: [
-          { name: 'check1', status: 'completed', conclusion: 'success' },
-          { name: 'check2', status: 'completed', conclusion: 'failed' }
-        ]
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.not.exists(err);
-      sha.should.be.eql(aSha);
-      done();
-    });
-  });
-
-  it.skip('should wait when one of the require checks is not completed', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: ['check1', 'check3']
-      },
-      getChecksForRefRes: null
-    });
-    const stub = sinon.stub(githubDummy, 'getChecksForRef');
-    stub.onFirstCall().callsArgWith(2, null, {
-      check_runs: [
-        { name: 'check1', status: 'completed', conclusion: 'success' },
-        { name: 'check2', status: 'completed', conclusion: 'failed' },
-        { name: 'check3', status: 'pending', conclusion: '' },
-      ]
-    });
-    stub.onSecondCall().callsArgWith(2, null, {
-      check_runs: [
-        { name: 'check1', status: 'completed', conclusion: 'success' },
-        { name: 'check2', status: 'completed', conclusion: 'failed' },
-        { name: 'check3', status: 'completed', conclusion: 'success' },
-      ]
-    });
-
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.not.exists(err);
-      sha.should.be.eql(aSha);
-      done();
-    });
-  });
-
-  it('should fail when one of the require checks failed', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: ['check1', 'check3']
-      },
-      getChecksForRefRes: {
-        check_runs: [
-          { name: 'check1', status: 'completed', conclusion: 'success' },
-          { name: 'check2', status: 'completed', conclusion: 'failed' },
-          { name: 'check3', status: 'completed', conclusion: 'failed' }
-        ]
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.exists(err);
-      err.message.should.be.eql('CHECKS_FAILED');
-      done();
-    });
-  });
-
-  it('should fail when the last execution of one of the require checks failed although previous succeeded', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: ['check1', 'check3']
-      },
-      getChecksForRefRes: {
-        check_runs: [
-          { name: 'check1', status: 'completed', conclusion: 'failed' },
-          { name: 'check2', status: 'completed', conclusion: 'failed' },
-          { name: 'check3', status: 'completed', conclusion: 'success' },
-          { name: 'check1', status: 'completed', conclusion: 'success' }
-        ]
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.exists(err);
-      err.message.should.be.eql('CHECKS_FAILED');
-      done();
-    });
-  });
-
-  it('should succeed when the last execution of all of the require checks succeeded although previous failed', (done) => {
-    const aSha = '123';
-    const githubDummy = createGithubDummy(null, {
-      getBranchRes: {
-        object: {
-          sha: aSha
-        }
-      },
-      getProtectedBranchRequiredStatusChecksRes: {
-        contexts: ['check1', 'check3']
-      },
-      getChecksForRefRes: {
-        check_runs: [
-          { name: 'check1', status: 'completed', conclusion: 'success' },
-          { name: 'check2', status: 'completed', conclusion: 'failed' },
-          { name: 'check3', status: 'completed', conclusion: 'success' },
-          { name: 'check1', status: 'completed', conclusion: 'failed' }
-        ]
-      }
-    });
-    const getBranchStatus = createGetBranchStatus(githubDummy, POLLING_INTERVAL_MS);
-
-    const repo = 'audienseCo';
-    const branch = 'staging';
-    getBranchStatus(repo, branch, (err, sha) => {
-      should.not.exists(err);
-      sha.should.be.eql(aSha);
-      done();
-    });
-  });
 
   function createGithubDummy(err, {
     getBranchRes,
-    getProtectedBranchRequiredStatusChecksRes,
-    getChecksForRefRes,
-    getProtectedBranchRequiredStatusChecksError
   }) {
-    var calls = [];
-    function called(name, cb) {
-      calls.push(name);
-    }
-    function wasCalled(name) {
-      return calls.includes(name)
-    }
     return {
-      getBranch: (repo, branch, cb) => called('getBranch', cb(err, getBranchRes)),
-      getProtectedBranchRequiredStatusChecks: (repo, branch, cb) => called('getProtectedBranchRequiredStatusChecks', cb(getProtectedBranchRequiredStatusChecksError, getProtectedBranchRequiredStatusChecksRes)),
-      getChecksForRef: (repo, ref, cb) => called('getChecksForRefRes', cb(err, getChecksForRefRes)),
-      wasCalled: wasCalled
+      getBranch: (repo, branch, cb) => cb(err, getBranchRes),
     };
   }
 });
